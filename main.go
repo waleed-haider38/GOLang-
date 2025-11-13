7d62c35086bce15c8db6d500e9d0e10e5687581c
Waleed Haider <waleedhaider988@gmail.com>
2025-11-13 12:40:07 +0500

    Add some booking app logic

--- main.go
+++ main.go
@@ -8,22 +8,46 @@ import "fmt" //fmt is package where we can use some built in functions like Prin
 func main() {
 	userName := "Waleed Haider"
 	userAge := 23
+
+	// fmt.Println(userAge)
+	// fmt.Println(&userAge)
 	// fmt.Printf("userName type is %T Age type is %T \n", userName, userAge)
 	fmt.Println("Welcom to my Go tutorial")
 	fmt.Printf("My name is %v \n", userName)
 	fmt.Printf("I am %v years old\n", userAge)
 	fmt.Println("Lets enjoy this journey together")
+	fmt.Println("Lets make Booking App")
 
-	// fmt.Println(userAge)
-	// fmt.Println(&userAge)
+	var remainingTickets uint = 50
+	var userTickets uint
+
+	fmt.Printf("Remainig Tickets are %v \n", remainingTickets)
 
 	// Taking Input from the user
 
-	var user string
+	var firstUser string
+	var lastUser string
+	var email string
 
 	fmt.Print("Enter your First Name: \n")
-	fmt.Scan(&user)
+	fmt.Scan(&firstUser)
 
-	fmt.Printf("User Name is %v", user)
+	fmt.Print("Enter your Last Name: \n")
+	fmt.Scan(&lastUser)
+
+	fmt.Print("Enter your email address: \n")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter the number of tickets you want to buy : \n")
+	fmt.Scan(&userTickets)
+
+	if userTickets <= remainingTickets {
+		remainingTickets = remainingTickets - userTickets
+	} else {
+		fmt.Println("Not enough tickets available!")
+	}
+
+	fmt.Printf("User Name is %v %v and his email address is %v \n", firstUser, lastUser, email)
+	fmt.Printf("Remaining Tickets are %v", remainingTickets)
 
 }
