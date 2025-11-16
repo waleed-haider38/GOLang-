b0e02b446b1c1b344866e5ec2edf71894ede618e
Waleed Haider <waleedhaider988@gmail.com>
2025-11-16 22:21:08 +0500

    add basic data

--- structs/mystruct.go
+++ structs/mystruct.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type School struct {
+	school_name  string
+	teacher_name string
+	student_name string
+	roll_no      uint
+}
+
 func main() {
-	MyStruct()
+	go MyStruct()
+
+	time.Sleep(1 * time.Second)
 
 	var greet []string
 
@@ -28,4 +38,14 @@ func main() {
 
 func MyStruct() {
 	fmt.Println("Here we will discuss about struct")
+	var std1 School
+	std1.school_name = "Scholar Public School"
+	std1.teacher_name = "Sir Sohail"
+	std1.student_name = "Muhammad Waleed Haider"
+	std1.roll_no = 11
+
+	fmt.Println(std1.school_name)
+	fmt.Println(std1.teacher_name)
+	fmt.Println(std1.student_name)
+	fmt.Println(std1.roll_no)
 }
